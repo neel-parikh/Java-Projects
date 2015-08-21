d6c901f8709c05387c2f7be1e089208438126364
Neel <parikhneelv@gmail.com>
2015-08-21 15:27:43 -0500

    Spring MVC

---
+++ computerscience/src/main/java/computerscience/HelloController.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package computerscience;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Rahul
+ *
+ */
+@Controller
+public class HelloController {
+	   @RequestMapping("/Hello")
+	   public String helloWorld(ModelMap model) {
+		  model.addAttribute("message","Hello This is my first spring mvc");
+	      return "HelloWorld";
+	   }
+	   
+	   @RequestMapping("/Welcome")
+	   public String helloWorld2(ModelMap model) {
+		  model.addAttribute("message","Welcome to spring mvc");
+	      return "HelloWorld2";
+	   }
+}
